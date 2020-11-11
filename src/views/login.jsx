f544a4b846f9de31eaa6866f302b1ce38dc81565
baozi <961263591@qq.com>
2020-11-11 09:33:40 +0800

    sss

---
+++ src/views/login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Button} from 'antd'
+
+function Login(){
+    return(
+        <div>
+            login
+            <Button  type="primary">点击</Button>
+        </div>
+    )
+}
+
+export default Login
