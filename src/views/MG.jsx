d171ee8d25621b74fae43a7b9e0569a21c034f98
wmh <1484103090@qq.com>
2020-11-12 10:15:29 +0800

    66

--- src/views/MG.jsx
+++ src/views/MG.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import request from '../utils/request'
+import proxy from '../setupProxy'
 
-function MG(){
-    return(
-        <div>手游</div>
-    )
+class MG extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            lists :[]
+        }
+    }
+
+    componentDidMount(){
+        
+    }
+   render(){
+        return(
+            <div>手游</div>
+        )
+   }
 }
 
 export default MG
