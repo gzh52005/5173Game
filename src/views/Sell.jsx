653da47220a929e9059bdcbcab34d019346d7015
wyy <1321682058@qq.com>
2020-11-12 13:59:31 +0800

    react

--- src/views/Sell.jsx
+++ src/views/Sell.jsx
@@ -6,32 +6,31 @@ import img from '../img/history.png'
 import hot2 from '../asset/image/hot2.png'
 import request from '../utils/request';
 import { Menu } from 'antd';
+const { SubMenu } = Menu;
 
 
-
+const num1 = <img className="hot" src={hot2}/>
 class Sell extends React.Component{
     state = {
-        tabs:[
-            { title: 'hot', key: 't1' },
-            { title: 'A', key: 't2' },
-            { title: 'B', key: 't3' },
-            { title: 'C', key: 't4' },
-            { title: 'D', key: 't5' },
-            { title: 'E', key: 't6' },
-            { title: 'F', key: 't7' },
-            { title: 'H', key: 't8' },
-            { title: 'I', key: 't9' },
-            { title: 'J', key: 't10' },
-            { title: 'K', key: 't11' },
-            { title: 'K', key: 't12' },
-            { title: 'K', key: 't13' },
-            { title: 'K', key: 't14' },
-        ],
-        num:''
+        tabs:[num1,"A","B","C","D","E","F","G","H","J","K","L","M","N","P","Q","R","S","T","W","X","Y","Z"],
+        num:'',
+        current: '0',
+        data:[]
     }
     onChange=(e)=>{
         this.setState({
             num:e.nativeEvent.selectedSegmentIndex
+        })
+        request.get('/homeApi/5173type',{
+            hot:"1",
+            gametype:this.state.num+1,
+            page:1,
+            pagesize:15
+        }).then(res=>{
+            this.setState({
+                data:res.data
+            })
+            
         })
         
     }
@@ -45,16 +44,23 @@ class Sell extends React.Component{
             gametype:"2",
             page:1,
             pagesize:15
+        }).then(res=>{
+            this.setState({
+                data:res.data
+            })
+            
         })
     }
+   
     render(){
+        const { current } = this.state;
         return(
             <div style={{height:"100%",display:"flex",flexDirection:"column"}}>
                <div>
-                <div className="selltop clear">
-                        <h1>我要卖</h1>
-                        <img src={img}/>
-                        <div className="search">搜索游戏</div>
+                    <div className="selltop clear">
+                            <h1>我要卖</h1>
+                            <img src={img}/>
+                            <div className="search">搜索游戏</div>
                     </div>
                     <WingBlank size="lg" className="sc-example">
                         <SegmentedControl 
@@ -63,32 +69,24 @@ class Sell extends React.Component{
                         onChange={this.onChange}
                         selectedIndex={this.state.num?this.state.num:0}/>
                     </WingBlank>
-               </div>
-
-
-               <Menu onClick={this.handleClick} selectedKeys={[current]} mode="horizontal">
-                    <Menu.Item key="mail" icon={<MailOutlined />}>
-                    Navigation One
-                    </Menu.Item>
-                    <Menu.Item key="app" disabled icon={<AppstoreOutlined />}>
-                    Navigation Two
-                    </Menu.Item>
-                    <SubMenu key="SubMenu" title="Navigation Three - Submenu">
-                    <Menu.ItemGroup title="Item 1">
-                        <Menu.Item key="setting:1">Option 1</Menu.Item>
-                        <Menu.Item key="setting:2">Option 2</Menu.Item>
-                    </Menu.ItemGroup>
-                    <Menu.ItemGroup title="Item 2">
-                        <Menu.Item key="setting:3">Option 3</Menu.Item>
-                        <Menu.Item key="setting:4">Option 4</Menu.Item>
-                    </Menu.ItemGroup>
-                    </SubMenu>
-                    <Menu.Item key="alipay">
-                    <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-                        Navigation Four - Link
-                    </a>
-                    </Menu.Item>
-      </Menu>
+                </div>
+                <div style={{overflow:"hidden",flex:"1",display:"flex",justifyContent:"space-between"}}>
+                    <div className="sell_main">
+                        <ul>
+                            {this.state.data.map((item,index)=>(<li key={index}>
+                                <img src={item.game_image_url} alt=""/>
+                            <p>{item.name}</p>
+                            </li>))}
+                        </ul>
+                    </div>
+                    <Menu onClick={this.handleClick} selectedKeys={[current]} mode="vertical">
+                    { this.state.tabs.map((item,index)=><Menu.Item key={index}>
+                        {item}
+                        </Menu.Item>)
+                    }
+                    
+                    </Menu>
+              </div>
         </div>
         )
     }
