8d35eba9cb4ea584720cc82c5a55a8b3053f83ba
e <1183330249@qq.com>
2020-11-13 14:39:02 +0800

    oneDetail

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Reg from './views/reg'
 import MineInfo from './views/MineInfo'
 import Details from './views/details.jsx'
 import search from './views/search'
+import OneDetail from './views/OneDetail.jsx'
 import {HomeOutlined,ShoppingCartOutlined,MessageOutlined,UserOutlined,TransactionOutlined,MessageFilled,HomeFilled } from '@ant-design/icons'
 import Order from './views/Order'
 class App extends React.Component {
@@ -90,6 +91,7 @@ class App extends React.Component {
             <Route path='/details' component={Details}/>
             <Route path='/mineinfo' component={MineInfo}/>
             <Route path='/search' component={search}/>
+            <Route path='/onedetail' component={OneDetail}/>
             <Redirect path="/" to="/home" exact/>
             <Route path='/order' component={Order}/>
             <Redirect path="/" to="/home" />
