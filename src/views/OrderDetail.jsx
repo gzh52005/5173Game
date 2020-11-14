1fea008733a23a7e649159b756b0e9aa0baa2fd7
baozi <961263591@qq.com>
2020-11-14 11:33:55 +0800

    ss

--- src/views/OrderDetail.jsx
+++ src/views/OrderDetail.jsx
@@ -1,19 +1,38 @@
-import React from 'react'
-import { List } from 'antd-mobile';
+import React,{useState,useCallback} from 'react'
+import { List,Icon } from 'antd-mobile';
+import '../layout/OrderDetail.scss'
+import img from '../assets/img/gamelogo.png'
 
 const Item = List.Item;
 const Brief = Item.Brief;
 function OrderDetail(props) {
-    let {path,data}=props.data.data
-    return (
-        <div>
-            {data}
-            <div className='error'>
+    console.log(props.data.data);
+    let {path,title}=props.data.data
 
+    let [isShow,changeShow]=useState(true)
+    console.log(isShow);
+
+    let change=useCallback(function(){
+        changeShow(false)
+    })
+    return (
+        <div className='OrderDetail'>
+            {title}
+            <div className='error'  style={{display:isShow?'flex':'none'}}>
+                <span className='iconBox' >
+                     <Icon onClick={change} type="cross-circle" color='#cf9a1b' size='xs'/>
+                </span>
+                <span style={{color:'#cf9a1b'}}>买家订单改版啦~~~游戏币、装备、帐号、手游、代练订单都可以在这里查看啦~~~</span>
+            </div>
+            <div className='noshopItem'>
+                <p><img src={img} alt=""/></p>
+                <h6>您没有任何订单</h6>
             </div>
             <div className={path}>
 
             </div>
+
+
         </div>
     )
 }
